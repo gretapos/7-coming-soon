3a54b7fb6139dbf1b568a8c95074901119083b3f
gretapos <86227872+gretapos@users.noreply.github.com>
2021-08-17 11:24:58 +0300

    Final progress bar validation #7

--- js/Data/progressBarData.js
+++ js/Data/progressBarData.js
@@ -2,6 +2,41 @@ const progressBarData = [
     {
         label: 'UX Design',
         value: 90,
+        hack: true,
+    },
+    {
+        label: 'UX Design',
+        value: 90,
+    },
+    'ertdyfh',
+    ['ewre', 452],
+    true,
+    null,
+    false,
+    () => { },
+    {
+        label: 'UX Design',
+        value: -90,
+    },
+    {
+        label: 'UX Design',
+        value: Infinity,
+    },
+    {
+        label: 'UX Design',
+        value: -Infinity,
+    },
+    {
+        label: 'UX Design',
+        value: NaN,
+    },
+    {
+        label: 'UX Design',
+        value: 990,
+    },
+    {
+        label: 'UX Design',
+        value: 77.7,
     },
     {},
     {
@@ -14,6 +49,10 @@ const progressBarData = [
     {
         label: 'Web Design',
         value: 86,
+    },
+    {
+        label: 'Web Design',
+        value: '86',
     },
     {
         label: [1, 2, 5],
--- js/main.js
+++ js/main.js
@@ -2,4 +2,3 @@ import { ProgressBars } from "./components/progress-bar/ProgressBars.js";
 import { progressBarData } from "./data/progressBarData.js";
 
 new ProgressBars('.left-column', progressBarData);
-new ProgressBars('.right-column', progressBarData);
